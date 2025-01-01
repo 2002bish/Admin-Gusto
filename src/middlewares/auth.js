0835d8b37e898b4d53b11bcd8445fcaa8b230d87
2002bish <bniroula707@gmail.com>
2025-01-01 12:59:56 +0545

    auth updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,6 @@ export const isAdmin = (req, res, next) => {
     if (!token) {
         return res.status(403).send('A token is required for authentication');
     }
-
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
             return res.status(401).send('Invalid Token');
